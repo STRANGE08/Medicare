e8de75301a83b1dfa000b58d8ed5a1260c5bfe45
Vikram Singh <singh.vkrm9079@gmail.com>
2022-01-05 17:51:21 +0530

    5 Jan evening

--- app/src/main/java/com/example/medicare/Activities/Doctor/My_Appointments_Activity.java
+++ app/src/main/java/com/example/medicare/Activities/Doctor/My_Appointments_Activity.java
@@ -2,19 +2,27 @@ package com.example.medicare.Activities.Doctor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.medicare.Adapters.Past_Appointment_Adapter;
+import com.example.medicare.Adapters.Upcoming_Appointments_Adapter;
 import com.example.medicare.MainActivity;
 import com.example.medicare.R;
 
 public class My_Appointments_Activity extends AppCompatActivity {
     String from;
 
+    TextView upcoming_appointment_tv, past_appointment_tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_appointments);
+
 
         from = getIntent().getStringExtra("from");
         findViewById(R.id.back).setOnClickListener(v -> {
@@ -27,6 +35,36 @@ public class My_Appointments_Activity extends AppCompatActivity {
                 finish();
             }
         });
+        RecyclerView my_appointments_rv = findViewById(R.id.my_appointments_rv);
+        my_appointments_rv
+                .setLayoutManager(
+                        new LinearLayoutManager(
+                                My_Appointments_Activity.this, RecyclerView.VERTICAL, false
+                        ));
+
+        my_appointments_rv.setAdapter(new Upcoming_Appointments_Adapter(My_Appointments_Activity.this));
+
+
+        upcoming_appointment_tv = findViewById(R.id.upcoming_appointment_tv);
+        past_appointment_tv = findViewById(R.id.past_appointment_tv);
+
+        upcoming_appointment_tv.setOnClickListener(v -> {
+            upcoming_appointment_tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+            past_appointment_tv.setTextColor(getResources().getColor(R.color.black));
+
+            my_appointments_rv.setAdapter(new Upcoming_Appointments_Adapter(My_Appointments_Activity.this));
+        });
+        past_appointment_tv.setOnClickListener(v -> {
+            past_appointment_tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+            upcoming_appointment_tv.setTextColor(getResources().getColor(R.color.black));
+            my_appointments_rv.setAdapter(new Past_Appointment_Adapter(My_Appointments_Activity.this));
+
+
+        });
+
+
+
+
     }
 
     public void onBackPressed() {
